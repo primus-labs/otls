171e5a761ed8fefed0ffd740beff8a046ecb027a
xiaojiqing <xiaojiqing@padolabs.org>
2023-03-05 19:54:15 -0800

    wss ok

--- websocket_test/compile.sh
+++ websocket_test/compile.sh
@@ -3,10 +3,12 @@ PWD=`pwd`
 EMOPENSSL=${PWD}/../../install_emopenssl
 OPENSSL=${PWD}/../../install_openssl
 
-gcc client.cpp -o client -g -lssl -lcrypto -ldl -pthread -I${OPENSSL}/include -L${OPENSSL}/lib/
-gcc server.cpp -o server -g -lssl -lcrypto -ldl -pthread -I${OPENSSL}/include -L${OPENSSL}/lib/
+g++ client.cpp -o client -g -lssl -lcrypto -ldl -pthread -I${OPENSSL}/include -L${OPENSSL}/lib/
+g++ server.cpp -o server -g -lssl -lcrypto -ldl -pthread -I${OPENSSL}/include -L${OPENSSL}/lib/
 g++ websocket_server.cpp sha1.cpp -o websocket_server -g -lssl -lcrypto -ldl -pthread -I${OPENSSL}/include -L${OPENSSL}/lib/
+g++ wss_server.cpp sha1.cpp wss.cpp -o wss_server -g -lssl -lcrypto -ldl -pthread -I${OPENSSL}/include -L${OPENSSL}/lib/
 
-emcc client.cpp  os.cpp -o client.html -g -lssl -lcrypto -ldl -lwebsocket.js -sWASM=1 -sERROR_ON_UNDEFINED_SYMBOLS=1 -sPROXY_POSIX_SOCKETS -sUSE_PTHREADS -sPROXY_TO_PTHREAD -sMAIN_MODULE -sREVERSE_DEPS=all --preload-file ca.crt -I${EMOPENSSL}/include -L${EMOPENSSL}/lib/
+em++ client.cpp  os.cpp -o client.html -g -lssl -lcrypto -ldl -lwebsocket.js -sWASM=1 -sERROR_ON_UNDEFINED_SYMBOLS=1 -sPROXY_POSIX_SOCKETS -sUSE_PTHREADS -sPROXY_TO_PTHREAD -sMAIN_MODULE -sREVERSE_DEPS=all --preload-file ca.crt -I${EMOPENSSL}/include -L${EMOPENSSL}/lib/
+em++ wss_client.cpp  os.cpp sha1.cpp wss.cpp -o wss_client.html -g -lssl -lcrypto -ldl -lwebsocket.js -sWASM=1 -sERROR_ON_UNDEFINED_SYMBOLS=1 -sPROXY_POSIX_SOCKETS -sUSE_PTHREADS -sPROXY_TO_PTHREAD -sMAIN_MODULE -sREVERSE_DEPS=all --preload-file ca.crt -I${EMOPENSSL}/include -L${EMOPENSSL}/lib/
 
-emcc websocket_client.cpp  os.cpp -o websocket_client.html -g -lssl -lcrypto -ldl -lwebsocket.js -sWASM=1 -sERROR_ON_UNDEFINED_SYMBOLS=1 -sPROXY_POSIX_SOCKETS -sUSE_PTHREADS -sPROXY_TO_PTHREAD -sMAIN_MODULE -sREVERSE_DEPS=all --preload-file ca.crt -I${EMOPENSSL}/include -L${EMOPENSSL}/lib/
+em++ websocket_client.cpp  os.cpp -o websocket_client.html -g -lssl -lcrypto -ldl -lwebsocket.js -sWASM=1 -sERROR_ON_UNDEFINED_SYMBOLS=1 -sPROXY_POSIX_SOCKETS -sUSE_PTHREADS -sPROXY_TO_PTHREAD -sMAIN_MODULE -sREVERSE_DEPS=all --preload-file ca.crt -I${EMOPENSSL}/include -L${EMOPENSSL}/lib/
