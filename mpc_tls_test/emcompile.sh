80aaa339109e2ae3d3f3b1274a14ba06e17750fa
xiaojiqing <xiaojiqing@padolabs.org>
2023-03-26 18:59:41 -0700

    update pado-emp

--- mpc_tls_test/emcompile.sh
+++ mpc_tls_test/emcompile.sh
@@ -1,19 +1,14 @@
 # sudo apt install libssl-dev
 
-EMOPENSSL_PATH=/home/ubuntu/test_openssl/otls/pado-emp/deps/install_wasm/
-EMOTLS_PATH=/home/ubuntu/test_openssl/otls/build_wasm
-EMEMP_PATH=/home/ubuntu/test_openssl/otls/pado-emp/build_wasm
+PWD=`pwd`
+EMOPENSSL_PATH=${PWD}/../pado-emp/deps/install_wasm/
+EMOTLS_PATH=${PWD}/../build_wasm
+EMEMP_PATH=${PWD}/../pado-emp/build_wasm
 
-CIRCUIT_FILE1="--preload-file ./cipher/circuit_files/aes128_ks.txt"
-CIRCUIT_FILE2="--preload-file ./cipher/circuit_files/aes128_with_ks.txt"
-CIRCUIT_FILE3="--preload-file ./ca.crt"
-CIRCUIT_FILE4="--preload-file ./server.crt"
-CIRCUIT_FILE5="--preload-file ./server.key"
-CIRCUIT_FILE6="--preload-file ./bristol_fashion/aes_128.txt"
+AES128_KS_FILE="--preload-file ./cipher/circuit_files/aes128_ks.txt"
+AES128_WITH_KS_FILE="--preload-file ./cipher/circuit_files/aes128_with_ks.txt"
+CA_FILE="--preload-file ./ca.crt"
+AES_128_FILE="--preload-file ./bristol_fashion/aes_128.txt"
 
-echo "compile server"
-# g++ -ggdb3 server.cpp  -L../build/mpc_tls -L${OPENSSL_PATH}/lib -lotls -lssl -lcrypto -pthread -ldl -o server -I../
 echo "compile client"
-em++ -s WASM=1 -ggdb3 client.cpp  os.cpp -L${EMOPENSSL_PATH}/lib -L${EMOTLS_PATH}/mpc_tls -L${EMEMP_PATH}/lib -lwebsocket.js -sPROXY_POSIX_SOCKETS -sUSE_PTHREADS -sPROXY_TO_PTHREAD -sFORCE_FILESYSTEM -sMAIN_MODULE -lotls -lemp-tool -lemp-zk -lssl -lcrypto -ldl -lrt -pthread -o client.html -I../ -I${EMOPENSSL_PATH}/include ${CIRCUIT_FILE1} ${CIRCUIT_FILE2} ${CIRCUIT_FILE3} ${CIRCUIT_FILE6} -s TOTAL_STACK=128MB -s INITIAL_MEMORY=256MB -s ALLOW_MEMORY_GROWTH=1
-echo "compile pado"
-# g++ -ggdb3 pado.cpp  -L../build/mpc_tls -L../../install_openssl/lib -L${EMP_PATH}/lib -lotls -lemp-tool -lemp-zk -lssl -lcrypto -pthread -ldl -o pado -I../ -I${OPENSSL_PATH}/include
+em++ -s WASM=1 -ggdb3 client.cpp  os.cpp -L${EMOPENSSL_PATH}/lib -L${EMOTLS_PATH}/mpc_tls -L${EMEMP_PATH}/lib -lwebsocket.js -sPROXY_POSIX_SOCKETS -sUSE_PTHREADS -sPROXY_TO_PTHREAD -sFORCE_FILESYSTEM -sMAIN_MODULE -lotls -lemp-tool -lemp-zk -lssl -lcrypto -ldl -lrt -pthread -o client.html -I../ -I${EMOPENSSL_PATH}/include ${AES128_KS_FILE} ${AES128_WITH_KS_FILE} ${AES_128_FILE} ${CA_FILE} -s TOTAL_STACK=128MB -s INITIAL_MEMORY=256MB -s ALLOW_MEMORY_GROWTH=1
