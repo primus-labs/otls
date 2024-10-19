9bf9c325a29246effb8810a8f581eab9ee13a508
Xiang Xie <xiexiangiscas@gmail.com>
2024-10-19 17:04:48 +0800

    add prove aes for the proxy model

--- test/prove_aes.cpp
+++ test/prove_aes.cpp
@@ -1,6 +1,7 @@
 #include "emp-zk/emp-zk.h"
 #include <iostream>
 #include "cipher/utils.h"
+#include "protocol/prove_aes.h"
 #if defined(__linux__)
 #include <sys/time.h>
 #include <sys/resource.h>
@@ -13,7 +14,35 @@
 using namespace std;
 using namespace emp;
 
-void prove_aes() {}
+bool prove_aes() {
+    unsigned char key_hex[] = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6,
+                               0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c};
+    reverse(key_hex, key_hex + 16);
+    Integer key(128, key_hex, ALICE);
+
+    unsigned char nounces[] = {0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96,
+                               0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a,
+                               0xae, 0x2d, 0x8a, 0x57, 0x1e, 0x03, 0xac, 0x9c,
+                               0x9e, 0xb7, 0x6f, 0xac, 0x45, 0xaf, 0x8e, 0x51};
+
+    unsigned char msgs[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+    unsigned char ctxts[] = {0x3a, 0xd7, 0x7b, 0xb4, 0x0d, 0x7a, 0x36, 0x60, 0xa8, 0x9e, 0xca,
+                             0xf3, 0x24, 0x66, 0xef, 0x97, 0xf5, 0xd3, 0xd5, 0x85, 0x03, 0xb9,
+                             0x69, 0x9d, 0xe7, 0x85, 0x89, 0x5a, 0x96, 0xfd, 0xba, 0xaf};
+
+    AESProver prover(key);
+    size_t len_bytes = sizeof(nounces);
+
+    Integer private_msg(len_bytes * 8, nounces, ALICE);
+
+    bool res = prover.prove_public_msgs(nounces, msgs, ctxts, len_bytes);
+    res ^= prover.prove_private_msgs(nounces, private_msg, ctxts, len_bytes);
+
+    return res;
+}
 
 const int threads = 1;
 int main(int argc, char** argv) {
@@ -31,7 +60,7 @@ int main(int argc, char** argv) {
     auto comm = io[0]->counter;
     auto rounds = io[0]->rounds;
 
-    setup_zk_bool<BoolIO<NetIO>>(ios, threads, party);
+    setup_proxy_protocol(ios, threads, party);
 
     cout << "setup time: " << emp::time_from(start) << " us" << endl;
     cout << "setup comm: " << (io[0]->counter - comm) * 1.0 / 1024 << " Kbytes" << endl;
@@ -39,31 +68,22 @@ int main(int argc, char** argv) {
 
     comm = io[0]->counter;
     rounds = io[0]->rounds - rounds;
-    init_files();
-
-    unsigned char* z = new unsigned char[16];
 
     start = emp::clock_start();
-
-    Integer key(128, 0, ALICE);
-    Integer expended_key = computeKS(key);
-    for (int i = 0; i < 64; ++i) {
-        Integer m(128, 0, ALICE);
-        Integer nounce(128, 0, PUBLIC);
-        Integer c = computeAES_KS(expended_key, nounce);
-        Integer out = (c ^ m);
-        out.reveal<unsigned char>((unsigned char*)z, PUBLIC);
+    bool res = prove_aes();
+    if (!res) {
+        error("prove error:\n");
     }
 
     cout << "zk AND gates: " << CircuitExecution::circ_exec->num_and() << endl;
 
-    bool cheated = finalize_zk_bool<BoolIO<NetIO>>();
+    bool cheated = finalize_proxy_protocol<BoolIO<NetIO>>();
+    if (cheated)
+        error("cheated\n");
 
     cout << "prove time: " << emp::time_from(start) << " us" << endl;
     cout << "prove comm: " << (io[0]->counter - comm) * 1.0 / 1024 << " Kbytes" << endl;
     cout << "prove rounds: " << (io[0]->rounds - rounds) << " rounds" << endl;
-    if (cheated)
-        error("cheated\n");
 #if defined(__linux__)
     struct rusage rusage;
     if (!getrusage(RUSAGE_SELF, &rusage))
@@ -81,7 +101,6 @@ int main(int argc, char** argv) {
     else
         std::cout << "[Mac]Query RSS failed" << std::endl;
 #endif
-    uninit_files();
 
     for (int i = 0; i < threads; ++i) {
         delete ios[i]->io;
