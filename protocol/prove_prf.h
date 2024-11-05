b5a41ac6d8c8ca36595dc4ba692a442df7e0e730
xiaojiqing <xiaojiqing@padolabs.org>
2024-11-04 19:07:00 -0800

    prove prf

--- protocol/prove_prf.h
+++ protocol/prove_prf.h
@@ -10,29 +10,7 @@
 
 #include "emp-tool/emp-tool.h"
 #include "cipher/utils.h"
-
-static unsigned char master_key_label[] = {"master secret"};
-static unsigned char key_expansion_label[] = {"key expansion"};
-static unsigned char client_finished_label[] = {"client finished"};
-static unsigned char server_finished_label[] = {"server finished"};
-static unsigned char extended_master_key_label[] = {"extended master secret"};
-
-static size_t master_key_label_length = sizeof(master_key_label) - 1;
-static size_t key_expansion_label_length = sizeof(key_expansion_label) - 1;
-static size_t client_finished_label_length = sizeof(client_finished_label) - 1;
-static size_t server_finished_label_length = sizeof(server_finished_label) - 1;
-static size_t extended_master_key_label_length = sizeof(extended_master_key_label) - 1;
-
-static const size_t master_key_length = 384 / 8;
-static const size_t expansion_key_length = 320 / 8;
-static const size_t finished_msg_length = 96 / 8;
-static const size_t tag_length = 16;
-static const size_t iv_length = 4;
-static const size_t key_length = 128 / 8;
-static const size_t extended_master_key_length = 384 / 8;
-
-static const size_t random_length = 256 / 8;
-static const size_t session_hash_length = 256 / 8;
+#include "protocol/handshake.h"
 
 /*
  * The PRF Prover
