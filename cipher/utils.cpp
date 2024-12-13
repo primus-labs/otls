b5cd0e9b29df950f8906127462cbdcd1a82bde14
ujnss <unlinunlin@163.com>
2024-12-13 07:54:27 +0000

    [app] support android and ios

--- cipher/utils.cpp
+++ cipher/utils.cpp
@@ -8,13 +8,22 @@ __thread BristolFormat *aes_ks = nullptr;
 __thread BristolFormat *aes_enc_ks = nullptr;
 #endif
 
+#if LOAD_CERT_FROM_FILE
 static const char* aes_ks_file = "cipher/circuit_files/aes128_ks.txt";
-
 static const char* aes_enc_file = "cipher/circuit_files/aes128_with_ks.txt";
+#else
+extern std::string aes128_ks_data;
+extern std::string aes128_with_ks_data;
+#endif
 
 void init_files() {
+    #if LOAD_CERT_FROM_FILE
     aes_ks = new BristolFormat(aes_ks_file);
     aes_enc_ks = new BristolFormat(aes_enc_file);
+    #else
+    aes_ks = new BristolFormat(aes128_ks_data);
+    aes_enc_ks = new BristolFormat(aes128_with_ks_data);
+    #endif
 }
 
 void uninit_files() {
